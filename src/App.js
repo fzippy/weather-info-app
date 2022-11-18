ce477d95d8a50ed66ac3f6fcab5db6da3c578e9a
fzippy <hk2011khan@gmail.com>
2022-11-18 18:45:00 +0000

    Added componet

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import logo from "./logo.svg";
 import "./App.css";
 import SearchBar from "./components/searchBar";
+import Card from "./components/card";
 
 function App() {
   return (
     <div className="container">
       <SearchBar />
+      <div className="cardContainer">
+        <Card></Card>
+      </div>
     </div>
   );
 }
