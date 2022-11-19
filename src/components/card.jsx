e77d9db6bc63ec49827136a07c585275daf35ed2
fzippy <hk2011khan@gmail.com>
2022-11-19 18:49:34 +0000

    Making the card dynamic

--- src/components/card.jsx
+++ src/components/card.jsx
@@ -1,68 +1,42 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-// const Card = function(){
-//     const [data, setCountrydata] = useState({hits:[]});
-//     useEffect( async () => {
-//         const fetchData = async()=>{
-//             const result = await axios('https://restcountries.com/v2/name/pakistan')
-//             setCountrydata(result.name)
-//         }
-//         fetchData();
 
-//     },[]);
-//     // const data = async()=>{
-//     //     await fetch("https://restcountries.com/v2/name/pakistan").then((response) =>response.json())
-//     //     console.log(data)
-//     // }
-//     return(
-//         <div className="card">
-//           <img src="" className="cardImg" alt="" />
-//           <p className="cardCountry">Pakistan</p>
-//           <p className="cardCountry">Population: 1 [the paki amar]</p>
-//           <p className="cardCountry">Weather: 69</p>
-//           <div className="row">
-//             <p></p>
-//             <div className="downArrow">↓</div>
-//           </div>
-//           <div className="row">
-//             <p>Weather info</p>
-//             <div className="downArrow">↓</div>
-//           </div>
-//         </div>
-//     )
-// }
-// export default Card
-function Card() {
-    const [data, setData] = useState(0);
+function Card(props) {
+    const [data, setData] = useState([]);
+    const [weather, setWeather] = useState([]);
   
     useEffect(() => {
       const fetchData = async () => {
         const result = await axios.get(
-          'https://restcountries.com/v2/name/somalia',
+          `https://restcountries.com/v2/name/${props.country}`,
         )
+        
 
         // console.log(result.data[0])
         setData(result.data[0]);
   
       };
   
-        fetchData();
+      fetchData();
 
     },[])
-    console.log(data)
+    // console.log(data)
     return (
         
         <div className="card">
-          <img src='' className="cardImg" alt="" />
-          <p className="cardCountry">{data.name}</p>
-          <p className="cardCountry">{data.population}</p>
-          <p className="cardCountry">Weather: 69</p>
-          <div className="row">
-            <p></p>
+          <div className='imgContainer cardImg'>
+            <img src={data.flag} className="cardFlag" alt="" />
+
+          </div>
+          <p className="cardCountry"> <strong>Name:</strong> {data.name}</p>
+          <p className="cardCountry"> <strong>Popultation:</strong> {data.population}</p>
+          <p className="cardCountry"> <strong>Weather:</strong> 69</p>
+          <div className="row" style={{margin:'10px 0'}}>
+            <p> <strong>More infomation about</strong> {data.name}</p>
             <div className="downArrow">↓</div>
           </div>
           <div className="row">
-            <p>Weather info</p>
+            <p><strong>More weather infomation</strong></p>
             <div className="downArrow">↓</div>
           </div>
         </div>
