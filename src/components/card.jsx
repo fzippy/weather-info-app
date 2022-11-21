41da2fa827e0ac10edb72a13edeaeb43b0bbf35c
fzippy <hk2011khan@gmail.com>
2022-11-21 19:56:42 +0000

    Changed api

--- src/components/card.jsx
+++ src/components/card.jsx
@@ -3,24 +3,23 @@ import axios from 'axios';
 
 function Card(props) {
 
-  const [data, setData] = useState([]);
-  const [weather, setWeather] = useState([]);
+  const [data, setData] = useState();
+  const [weather, setWeather] = useState();
   
 
       const fetchCountryData = async ()=>{
         const latAndLong = await axios.get(
           `http://api.openweathermap.org/geo/1.0/direct?q=${props.country}&appid=3001171d64b6cd2480ec2312c708d488`
         )
-        // getcoordinates([])
         fetchData(latAndLong.data[0].country)
         fetchWeather(latAndLong.data[0].lat, latAndLong.data[0].lon)
         
       }
       const fetchData = async (country) => {
         const result = await axios.get(
-          `https://restcountries.com/v2/name/${country}`,
+          `https://restcountries.com/v3.1/alpha/${country}`
         )
-        // console.log(latAndLong.data[0].lat)
+        console.log(result.data[0])
         setData(result.data[0]);
 
       };
@@ -28,9 +27,6 @@ function Card(props) {
         const getWeather = await axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=3001171d64b6cd2480ec2312c708d488`)
         setWeather(getWeather.data)
       }
-      // fetchData(coordinates[2]);
-
-
 
     useEffect(() => {
       console.log("MOUNTED")
@@ -46,14 +42,15 @@ function Card(props) {
         
         <div className="card">
           <div className='imgContainer cardImg'>
-            <img src={data.flag} className="cardFlag" alt="" />
+            <img src={data?.flags.svg} className="cardFlag" alt="" />
 
           </div>
-          <p className="cardCountry"> <strong>Name:</strong> {data.name}</p>
-          <p className="cardCountry"> <strong>Population:</strong> {data.population}</p>
-          <p className="cardCountry"> <strong>Weather:</strong> {weather.main?.temp - 273.15} °C</p>
+          <p className="cardCountry"> <strong>Search:</strong> {props.city}</p>
+          <p className="cardCountry"> <strong>Country:</strong> {data?.name.common}</p>
+          {/* <p className="cardCountry"> <strong>Population:</strong> {data.name.population}</p> */}
+          <p className="cardCountry"> <strong>Weather:</strong> {weather?.main.temp - 273.15} °C</p>
           <div className="row" style={{margin:'10px 0'}}>
-            <p> <strong>More infomation about</strong> {data.name}</p>
+            <p><strong>More infomation about</strong> {data?.name.common}</p>
             <div className="downArrow">↓</div>
           </div>
           <div className="row">
