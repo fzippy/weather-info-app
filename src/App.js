28ee5a6c6b74dfaad6b8f8695f7b60240472bd23
fzippy <hk2011khan@gmail.com>
2022-11-21 19:56:30 +0000

    Loop over the array to return different cards

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
-import logo from "./logo.svg";
+import React, { useState } from "react";
 import "./App.css";
 import SearchBar from "./components/searchBar";
 import Card from "./components/card";
 
 function App() {
+  const [countriesArray, updateCountries] = useState([]);
+  const array = ["hello"];
+  const getData = (data) => {
+    updateCountries([...countriesArray, data]);
+    console.log("data is this:" + data);
+    console.log(countriesArray);
+  };
+  const dataArray = countriesArray.map((item, i) => {
+    return <Card country={item} city={item} key={i} />;
+  });
+
   return (
     <div className="container">
-      <SearchBar />
+      <SearchBar onLicks={getData} />
       <div className="cardContainer">
-        <Card country="Slough"></Card>
+        {dataArray}
         {/* <Card country="United Kingdom"></Card> */}
       </div>
     </div>
