aba8a8a7957ab3f5ad254124aef1130b45db44b5
fzippy <hk2011khan@gmail.com>
2022-11-21 20:37:48 +0000

    Added population

--- src/components/card.jsx
+++ src/components/card.jsx
@@ -7,26 +7,26 @@ function Card(props) {
   const [weather, setWeather] = useState();
   
 
-      const fetchCountryData = async ()=>{
-        const latAndLong = await axios.get(
-          `http://api.openweathermap.org/geo/1.0/direct?q=${props.country}&appid=3001171d64b6cd2480ec2312c708d488`
-        )
-        fetchData(latAndLong.data[0].country)
-        fetchWeather(latAndLong.data[0].lat, latAndLong.data[0].lon)
-        
-      }
-      const fetchData = async (country) => {
-        const result = await axios.get(
-          `https://restcountries.com/v3.1/alpha/${country}`
-        )
-        console.log(result.data[0])
-        setData(result.data[0]);
+    const fetchCountryData = async ()=>{
+      const latAndLong = await axios.get(
+        `http://api.openweathermap.org/geo/1.0/direct?q=${props.country}&appid=3001171d64b6cd2480ec2312c708d488`
+      )
+      fetchData(latAndLong.data[0].country)
+      fetchWeather(latAndLong.data[0].lat, latAndLong.data[0].lon)
+      
+    }
+    const fetchData = async (country) => {
+      const result = await axios.get(
+        `https://restcountries.com/v3.1/alpha/${country}`
+      )
+      console.log(result.data[0])
+      setData(result.data[0]);
 
-      };
-      const fetchWeather = async(lat, lon)=>{
-        const getWeather = await axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=3001171d64b6cd2480ec2312c708d488`)
-        setWeather(getWeather.data)
-      }
+    };
+    const fetchWeather = async(lat, lon)=>{
+      const getWeather = await axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=3001171d64b6cd2480ec2312c708d488`)
+      setWeather(getWeather.data)
+    }
 
     useEffect(() => {
       console.log("MOUNTED")
@@ -34,31 +34,25 @@ function Card(props) {
       
 
     }, [])
-    // const metricWeather = function(){
-    //   return weather - 
-    // }
-    console.log() 
     return (
-        
-        <div className="card">
-          <div className='imgContainer cardImg'>
-            <img src={data?.flags.svg} className="cardFlag" alt="" />
+      <div className="card">
+        <div className='imgContainer cardImg'>
+          <img src={data?.flags.svg} className="cardFlag" alt="" />
 
-          </div>
-          <p className="cardCountry"> <strong>Search:</strong> {props.city}</p>
-          <p className="cardCountry"> <strong>Country:</strong> {data?.name.common}</p>
-          {/* <p className="cardCountry"> <strong>Population:</strong> {data.name.population}</p> */}
-          <p className="cardCountry"> <strong>Weather:</strong> {weather?.main.temp - 273.15} °C</p>
-          <div className="row" style={{margin:'10px 0'}}>
-            <p><strong>More infomation about</strong> {data?.name.common}</p>
-            <div className="downArrow">↓</div>
-          </div>
-          <div className="row">
-            <p><strong>More weather infomation</strong></p>
-            <div className="downArrow">↓</div>
-          </div>
         </div>
-    
+        <p className="cardCountry"> <strong>Search:</strong> {props.city}</p>
+        <p className="cardCountry"> <strong>Country:</strong> {data?.name.common}</p>
+        <p className="cardCountry"> <strong>Population:</strong> {data?.population}</p>
+        <p className="cardCountry"> <strong>Weather:</strong> {weather?.main.temp - 273.15} °C</p>
+        <div className="row" style={{margin:'10px 0'}}>
+          <p><strong>More infomation about</strong> {data?.name.common}</p>
+          <div className="downArrow">↓</div>
+        </div>
+        <div className="row">
+          <p><strong>More weather infomation</strong></p>
+          <div className="downArrow">↓</div>
+        </div>
+      </div>
     );
   }
   
