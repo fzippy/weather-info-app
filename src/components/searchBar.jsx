9c445b351545781aabdc8516fa66d8170557df93
fzippy <hk2011khan@gmail.com>
2022-11-21 19:57:20 +0000

    Made the search bar work with props

--- src/components/searchBar.jsx
+++ src/components/searchBar.jsx
@@ -1,12 +1,26 @@
-const SearchBar = function(){
+import React, { useState } from 'react';
+
+const SearchBar = function(props){
+  const [inputValue, setInput] = useState('')
+  const onChangeValue = (e)=>{
+    setInput(e.target.value)
+  }
+  const addSearch = ()=>{
+    props.onLicks(inputValue)
+    
+  }
     return(
         <div className="formInputContainer">
         <input
           type="text"
           className="inputField"
           placeholder="Search city or country"
+          name="listOfCountriesInput"
+          id="countriesInput"
+          value={inputValue}
+          onChange={onChangeValue}
         />
-        <div className="searchBtn"></div>
+        <div className="searchBtn" onClick={addSearch} ></div>
       </div>
     )
 }
